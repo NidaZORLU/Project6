be77b810d8d3f97f89e6e4c5dd7e3cda1f03bd1b
NidaZORLU <nidazorlunz@hotmail.com>
2021-09-08 09:58:13 +0300

    add gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+const {
+    src,
+    dest,
+    parallel,
+    series,
+    watch
+} = require('gulp');
+
+const uglify = require('gulp-uglify');
+const autoprefixer = require('gulp-autoprefixer');
+const cssnano = require('gulp-cssnano');
+const concat = require('gulp-concat');
+const changed = require('gulp-changed');
+const browsersync = require('browser-sync').create();
+const del = require('del')
+
+const dist = './dist'
+const source = './src';
+
+
+function js() {
+    return src(source + '/assets/js/script.js')
+        .pipe(changed(source))
+        .pipe(concat('script.js'))
+        .pipe(uglify())
+        .pipe(dest(dist + '/assets/js'))
+        .pipe(browsersync.stream());
+}
+
+
+function clean() {
+    return del([dist + '**', '!' + dist])
+}
+
+
+function css() {
+    return src(source + '/assets/css/style.css ')
+        .pipe(changed(source))
+        .pipe(autoprefixer({
+            overrideBrowserslist: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(cssnano())
+        .pipe(dest(dist + '/assets/css'))
+        .pipe(browsersync.stream());
+}
+
+
+function img() {
+    return src(source + '/assets/img/*')
+        //.pipe(imagemin())
+        .pipe(dest(dist + '/assets/img'));
+}
+
+function htmlfiles() {
+    return src(source + '/index.html')
+        //.pipe(imagemin())
+        .pipe(dest(dist));
+}
+
+function watchFiles() {
+    watch(source + '/assets/css/style.css', css);
+    watch(source + '/asset/js/script.js', js);
+    watch('/assets/img', img);
+    watch(source + '/index.html', htmlfiles)
+}
+
+
+function browserSync() {
+    browsersync.init({
+        server: {
+            baseDir: dist
+        },
+        port: 3000
+    });
+}
+
+exports.watch = series(js, css, img, htmlfiles, parallel(watchFiles, browserSync));
+exports.default = series(clean, parallel(js, css, img, htmlfiles));
